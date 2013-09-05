fd89591a5060067c8aa1f051bdd7423f865de5fb
Dan Smith <dan.smith@DanSmiths-MacBook-Pro.local>
2013-09-05 00:23:25 -0400

    ie test

--- basic-example.rb
+++ basic-example.rb
@@ -7,7 +7,30 @@ class ExampleTest < Test::Unit::TestCase
         caps = Selenium::WebDriver::Remote::Capabilities.firefox
         caps.version = "5"
         caps.platform = :XP
-        caps[:name] = "Testing Selenium 2 with Ruby on Sauce"
+        caps[:name] = "Firefox Testing Selenium 2 with Ruby on Sauce"
+
+        @driver = Selenium::WebDriver.for(
+          :remote,
+          :url => "http://dsmithco_github:197fbca4-bf44-46dc-b2e4-8185ed8259d5@ondemand.saucelabs.com:80/wd/hub",
+          :desired_capabilities => caps)
+    end
+
+    def test_sauce
+        @driver.navigate.to "http://angrails.herokuapp.com/"
+        assert @driver.title.include?("AngularRails4")
+    end
+
+    def teardown
+        @driver.quit
+    end
+end
+
+class ExampleTestIE < Test::Unit::TestCase
+    def setup
+        caps = Selenium::WebDriver::Remote::Capabilities.ie
+        caps.version = "8"
+        caps.platform = :XP
+        caps[:name] = "IE Testing Selenium 2 with Ruby on Sauce"
 
         @driver = Selenium::WebDriver.for(
           :remote,
